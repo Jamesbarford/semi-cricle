069289e0a73340c2665cc2d3b0f59a833fe3e166
Jamesbarford <jamesbarfordevans@gmail.com>
2021-06-11 20:22:21 +0100

    get envvars, get all pipelines for project. Updated readme

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # WIP: semi-circle
-
-A simple circle ci command line application for getting the status of pipelines. Can be used in a loop to perform simple polling and piped into `jq` for queriying
+A simple circle ci command line application for getting the status of pipelines. Can be used in a loop to perform simple polling and piped into `jq` for querying
 
 # Command line arguments
-
 - `-b` Name of branch
 - `-p` Name of project
 - `-c` Name of company
@@ -14,8 +12,10 @@ A simple circle ci command line application for getting the status of pipelines.
 # Supported Methods
 The following methods are supported:
 - `get_branch_pipeline` returns pipeline information
-- `get_workflow` pipeline id, returns workflow information for the given pipeline id and branch
+- `get_workflow` returns workflow information for the given pipeline id and branch
 - `get_my_branch_pipeline` returns only your pipelines for a branch
+- `list_envionment_variables` returns a list of Environment Variables in circleci, the values are masked
+- `get_my_pipelines` returns all of your pipelines for a project, for all branches
 
 __Examples:__
 
@@ -39,4 +39,26 @@ In order to obtain the pipeline id, you will probably need to run the above comm
 ./semi_circle.sh \
 	-i 13123-asddwe-12313-dsada \
 	-m get_workflow
+```
+
+`list_envionment_variables`
+
+Gets all teh environment variables for a project, the values are masked
+
+```sh
+./semi_circle.sh \
+  -p amazing_project \
+  -c wow_company \
+  -m list_envionment_variables
+```
+
+`get_my_pipelines`
+
+Returns the latest 250 pipelines for a project listed with the latest first
+
+```sh
+./semi_circle.sh \
+  -p amazing_project \
+  -c wow_company \
+  -m get_my_pipelines
 ```
--- semi_circle.sh
+++ semi_circle.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 BASE_URL="https://circleci.com/api/v2"
+BASE_URL_V1="https://circleci.com/api/v1.1"
 VCS="gh"
 PROJECT_SLUG=
 PIPELINE_ID=
@@ -11,13 +12,6 @@ BRANCH=
 
 FMT_BOLD=$(tput bold)
 FMT_NORMAL=$(tput sgr0)
-
-function check_circle_token() {
-    if [ -z $CIRCLE_TOKEN ]; then
-        echo "'CIRCLE_TOKEN must be set'"
-        exit 1
-    fi
-}
 
 function print_usage() {
     cat <<EOH
@@ -62,17 +56,41 @@ Methods:
         Returns:
             Workflow in JSON for a given pipeline id, contains the status i.e running, success, failed
 
+    ${FMT_BOLD}list_envionment_variables${FMT_NORMAL}
+        Usage:
+            $0 -p <project> -b <branch> -c <company> -m list_envionment_variables
+        Returns:
+            List of environment variables with values masked
+    
+    ${FMT_BOLD}get_my_pipelines${FMT_NORMAL}
+        Usage:
+            $0 -p <project> -b <branch> -c <company> -m get_my_pipelines
+        Returns:
+            A list of all your pipelines for a project for every branch you've used/created
+            
+
 Additional:
     - ${FMT_BOLD}'CIRCLE_TOKEN'${FMT_NORMAL} must be set to authorise api calls to circleci
 EOH
 exit 0
 }
 
+function check_circle_token() {
+    if [ -z $CIRCLE_TOKEN ]; then
+        echo "'CIRCLE_TOKEN must be set'"
+        exit 1
+    fi
+}
+
+function check_branch() {
+    if [ -z $BRANCH ]; then
+        echo "-b <branch> must be set"
+        exit 1
+    fi
+}
+
 function create_slug() {
     local MISSING=()
-    if [ -z $BRANCH ]; then
-        MISSING+=("-b <branch> must be set")
-    fi
 
     if [ -z $PROJECT ]; then
         MISSING+=("-p <project> must be set")
@@ -81,6 +99,7 @@ function create_slug() {
     if [ -z $COMPANY ]; then
         MISSING+=("-c <company> must be set")
     fi
+
     if [ ${#MISSING[@]} == 0 ]; then
         PROJECT_SLUG="$VCS/$COMPANY/$PROJECT"
     else
@@ -117,6 +136,7 @@ function get_args() {
             ;;
             h)
                 print_usage
+                exit 0
             ;;
             *)
                 echo "Unknown argument: $ARG"
@@ -126,11 +146,32 @@ function get_args() {
     done
 }
 
+function get_my_pipelines() {
+    : '
+        Return all of your pipelines
+    '
+    create_slug
+    curl -s \
+        --url "$BASE_URL/project/$PROJECT_SLUG/pipeline/mine" \
+        --header "Circle-Token: $CIRCLE_TOKEN"
+}
+
+function list_envionment_variables() {
+    : '
+        Returns a list of environment variables for a project
+    '
+    create_slug
+    curl -s \
+        --url "$BASE_URL_V1/project/$PROJECT_SLUG/envvar" \
+        --header "Circle-Token: $CIRCLE_TOKEN"
+}
+
 function get_my_branch_pipeline() {
     : '
         Return first page of your pipelines for branch
     '
     create_slug
+    check_branch
     curl -s \
         --url "$BASE_URL/project/$PROJECT_SLUG/pipeline/mine?branch=$BRANCH" \
         --header "Circle-Token: $CIRCLE_TOKEN"
@@ -141,6 +182,7 @@ function get_branch_pipeline() {
         Return first page of pipelines for branch
     '
     create_slug
+    check_branch
     curl -s \
         --url "$BASE_URL/project/$PROJECT_SLUG/pipeline?branch=$BRANCH" \
         --header "Circle-Token: $CIRCLE_TOKEN"
@@ -168,8 +210,16 @@ function call_method() {
         exit 1
     fi
     case $1 in
+        "get_my_pipelines")
+            get_my_pipelines
+            exit 0
+        ;;
         "get_workflow")
             get_workflow
+            exit 0
+        ;;
+        "list_envionment_variables")
+            list_envionment_variables
             exit 0
         ;;
         "get_branch_pipeline")
