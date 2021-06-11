e2e8505e204632f30c4e29b77b8b253d818941d4
Jamesbarford <jamesbarfordevans@gmail.com>
2021-06-11 18:56:16 +0100

    added help method, cleaned up command argument parsing

--- semi_circle.sh
+++ semi_circle.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 BASE_URL="https://circleci.com/api/v2"
-COMPANY=
-PROJECT=
 VCS="gh"
+PROJECT_SLUG=
+PIPELINE_ID=
+PROJECT=
+COMPANY=
 METHOD=
 BRANCH=
-PIPELINE_ID=
-PIPELINE_NUMBER=
-PROJECT_SLUG=
+
+FMT_BOLD=$(tput bold)
+FMT_NORMAL=$(tput sgr0)
 
 function check_circle_token() {
     if [ -z $CIRCLE_TOKEN ]; then
@@ -17,82 +19,118 @@ function check_circle_token() {
     fi
 }
 
-function check_arg() {
-    if [ -z $1 ]; then
-        print_usage
-    fi
-}
-
 function print_usage() {
     cat <<EOH
-Usage: $0 -p <project> -b <branch> -c <company> -m <method> -i <pipeline_id> [-v --vcs <version control>]
+Usage:
+    $0 -p <project> -b <branch> -c <company> -m <method> -i <pipeline_id> [-v --vcs <version control>] -h
 
 Where:
-    - project:           Name of your project
-    - branch:            Name of git branch
-    - company:           Name of company
-    - version control:   Version control, defaults to github
-    - method:            The method to call: get_branch_pipeline, get_latest_workflow
-    - pipeline id:       String id of pipeline
+    ${FMT_BOLD}-p <project>:${FMT_NORMAL}
+            Name of your project
+
+    ${FMT_BOLD}-b <branch>:${FMT_NORMAL}
+            Name of git branch
+
+    ${FMT_BOLD}-c <company>:${FMT_NORMAL}
+            Name of company
+
+    ${FMT_BOLD}-v <version_control>:${FMT_NORMAL}
+            Version control, defaults to github -> 'gh'
+
+    ${FMT_BOLD}-i <pipeline_id>:${FMT_NORMAL}
+            String id of pipeline  
+
+    ${FMT_BOLD}-m <method>:${FMT_NORMAL}
+            The method to call
+
+Methods:
+    ${FMT_BOLD}get_branch_pipeline${FMT_NORMAL}
+        Usage:
+            $0 -p <project> -b <branch> -c <company> -m get_branch_pipeline
+        Returns:
+            returns latest pipelines as a JSON array for a given branch, project and company
+      
+    ${FMT_BOLD}get_my_branch_pipeline${FMT_NORMAL}
+        Usage:
+            $0 -p <project> -b <branch> -c <company> -m get_my_branch_pipeline
+        Returns:
+            Your latest pipelines as a JSON array
+
+    ${FMT_BOLD}get_workflow${FMT_NORMAL}
+        Usage:
+            $0 -i <pipeline_id> -m get_workflow
+        Returns:
+            Workflow in JSON for a given pipeline id, contains the status i.e running, success, failed
 
 Additional:
-    - 'CIRCLE_TOKEN' must be set to authorise api calls to circleci
+    - ${FMT_BOLD}'CIRCLE_TOKEN'${FMT_NORMAL} must be set to authorise api calls to circleci
 EOH
 exit 0
 }
 
-function get_args() {
-    POSITIONAL=()
-    while [[ $# -gt 0 ]]
-    do
-    key="$1"
+function create_slug() {
+    local MISSING=()
+    if [ -z $BRANCH ]; then
+        MISSING+=("-b <branch> must be set")
+    fi
 
-    case $key in
-        -i|--pipeline_id)
-        PIPELINE_ID="$2"
-        shift
-        shift
-        ;;
-        -p|--project)
-        PROJECT="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        -c|--company)
-        COMPANY="$2"
-        shift
-        shift
-        ;;
-        -b|--branch)
-        BRANCH="$2"
-        shift
-        shift
-        ;;
-        -m|--method)
-        METHOD="$2"
-        shift
-        shift
-        ;;
-        --default)
-        DEFAULT=YES
-        shift # past argument
-        ;;
-        *)    # unknown option
-        POSITIONAL+=("$1") # save it in an array for later
-        shift # past argument
-        ;;
-    esac
+    if [ -z $PROJECT ]; then
+        MISSING+=("-p <project> must be set")
+    fi
+
+    if [ -z $COMPANY ]; then
+        MISSING+=("-c <company> must be set")
+    fi
+    if [ ${#MISSING[@]} == 0 ]; then
+        PROJECT_SLUG="$VCS/$COMPANY/$PROJECT"
+    else
+        for MISS in "${MISSING[@]}"; do
+            echo $MISS
+        done
+        exit 1
+    fi
+}
+
+function get_args() {
+    if [ $1 == "-h" ]; then
+        print_usage
+    fi
+    while getopts "h:i:p:c:b:m:" ARG; do
+        case $ARG in
+            i)
+                PIPELINE_ID=$OPTARG
+            ;;
+            p)
+                PROJECT=$OPTARG
+            ;;
+            c)
+                COMPANY=$OPTARG
+            ;;
+            b)
+                BRANCH=$OPTARG
+            ;;
+            m)
+                METHOD=$OPTARG
+            ;;
+            v)
+                VCS=$OPTARG
+            ;;
+            h)
+                print_usage
+            ;;
+            *)
+                echo "Unknown argument: $ARG"
+                exit 1
+            ;;
+        esac
     done
-    check_arg $BRANCH
-    check_arg $PROJECT
-    check_arg $COMPANY
-    PROJECT_SLUG="$VCS/$COMPANY/$PROJECT"
 }
 
 function get_my_branch_pipeline() {
     : '
         Return first page of your pipelines for branch
     '
+    create_slug
     curl -s \
         --url "$BASE_URL/project/$PROJECT_SLUG/pipeline/mine?branch=$BRANCH" \
         --header "Circle-Token: $CIRCLE_TOKEN"
@@ -102,6 +140,7 @@ function get_branch_pipeline() {
     : '
         Return first page of pipelines for branch
     '
+    create_slug
     curl -s \
         --url "$BASE_URL/project/$PROJECT_SLUG/pipeline?branch=$BRANCH" \
         --header "Circle-Token: $CIRCLE_TOKEN"
@@ -111,6 +150,10 @@ function get_workflow() {
     : '
         Return specific information about workflow for a unique pipeline id
     '
+    if [ -z $PIPELINE_ID ]; then
+        echo "-i <pipeline_id> missing"
+        exit 1
+    fi
     curl -s \
         --url "$BASE_URL/pipeline/$PIPELINE_ID/workflow" \
         --header "Circle-Token: $CIRCLE_TOKEN"
@@ -120,25 +163,33 @@ function call_method() {
     : '
         Execute method from comand line
     '
+    if [ -z $1 ]; then
+        echo "-m <method> must be set"
+        exit 1
+    fi
     case $1 in
         "get_workflow")
-        get_workflow
-        exit 0
+            get_workflow
+            exit 0
         ;;
         "get_branch_pipeline")
-        get_branch_pipeline
-        exit 0
+            get_branch_pipeline
+            exit 0
         ;;
         "get_my_branch_pipeline")
-        get_my_branch_pipeline
-        exit 0
+            get_my_branch_pipeline
+            exit 0
         ;;
         *)
-        echo '{"error": "No method:' "$1" 'found" }'
+            echo '{"error": "No method:' "$1" 'found" }'
+        ;;
     esac
 }
 
 function execute() {
+    if [ -z $1 ]; then
+        print_usage
+    fi
     check_circle_token
     get_args $@
     call_method $METHOD
